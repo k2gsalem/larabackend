37956356c6950180ae31e212b13102b3d980a5f2
k2gsalem <k2gsalem@gmail.com>
2025-10-30 16:52:29 +0530

    fi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,6 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     ->by($request->user()?->getAuthIdentifier() ?? $request->ip()),
             ];
         });
+
         $middleware->alias([
             'tenant.user' => EnsureTenantUser::class,
             'tenant.initialize' => InitializeTenancyByRequestData::class,
@@ -40,6 +41,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->group('tenant-api', [
             InitializeTenancyByRequestData::class,
+            PreventAccessFromCentralDomains::class,
             'auth:sanctum',
             EnsureTenantUser::class,
         ]);
